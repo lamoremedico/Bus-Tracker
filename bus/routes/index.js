f2f757a8fadce8c57cc23b63044ed19f4acca9b0
Nick <lamoremedico@gmail.com>
2016-01-27 15:12:05 -0500

    Created Page after Login and implemented Log out function redirecting to initial Log In Page

--- bus/routes/index.js
+++ bus/routes/index.js
@@ -3,18 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Log In' });
+});
+
+router.get('/login', function(req, res, next) {
+	res.redirect('/');
 });
 
 router.post('/login', function(req, res, next) {
 	var user = req.param('user');
 	var pswd = req.param('pswd');
 	if (user === "n" && pswd === "10")  {
+		console.log("Username: " + user + "\nPassword: " + pswd);
+		//console.log(req);
+		//console.log(res);
 		res.render('anotherpage', { title: 'Welcome' });
 	}
 	else {
 		res.redirect('/');
 	}
+});
+
+router.get('/logout', function(req, res, next) {
+	res.render('index', { title: 'Log Out' });
 });
 
 module.exports = router;
