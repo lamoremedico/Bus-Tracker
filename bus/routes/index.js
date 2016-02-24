05f4af191cff1e9e72dcd0f2f37b9233e836ca20
Nick <lamoremedico@gmail.com>
2016-02-24 12:52:08 -0500

    Tried to implement cookie authentication - not sure if it works

--- bus/routes/index.js
+++ bus/routes/index.js
@@ -9,43 +9,39 @@ app.use(cookieParser())
  
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-  //if (req.cookies.remember) {
-  	//res.render('anotherpage', {title: 'Welcome'});
-  	//}
-  	//else {
-  res.render('index', { title: 'Log In' });
-   //   }
-});
-
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Log In' });
+  if (req.cookies.remember) {
+  	res.render('anotherpage', {title: 'Welcome'});
+  	}
+  	else {
+  res.render('index', { title: 'Log In' });
+     }
 });
 
-//app.get('/', function(req, res, next) {
-//	res.send(req.cookies.name);
-//});
-
+// GET Login page but if no cookies redirect to home page
 router.get('/login', function(req, res, next) {
+	if (req.cookies.remember) {
+  	res.render('anotherpage', {title: 'Welcome'});
+  }
+  	else {
 	res.redirect('/');
+   }
 });
 
+//If login info is valid - send to next page
 router.post('/login', function(req, res, next) {
 	var user = req.param('user');
 	var pswd = req.param('pswd');
 	if (user === "namore" && pswd === "200192")  {
 		console.log("Username: " + user + "\nPassword: " + pswd);
+		res.cookie( 'name', user)
+		console.log("Cookie = " + user);
 		res.render('anotherpage', { title: 'Welcome' });
 	}
 	else {
 		res.redirect('/');
 	}
 });
-
-//app.post('/login',function(req, res){
-     //res.cookie( 'name', req.param.user)
-     	//.send('Cookie is set');
-//});
 
 router.get('/logout', function(req, res, next) {
 	res.render('logout', { title: 'Log Out' });
