07bc5ec425e84dccf249e498a968667b30452e9a
Peter Lytle <pete@bluecampaigns.com>
2016-02-28 13:23:30 -0500

    added maps

--- bus/public/static/js/route_watch.js
+++ bus/public/static/js/route_watch.js
@@ -3,13 +3,38 @@ function startUpdater(){
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://127.0.0.1:3001/info", true);
     xhttp.send();
+
     setTimeout(function(){
       var text = xhttp.responseText;
       var point = JSON.parse(text)
-      document.getElementById('bus_1_info').innerHTML = 'lon: ' + point.point[0] + ' - lat: ' + point.point[1];
+
+      var lat = point[0];
+      var lon = point[1];
+
+      document.getElementById('bus_1_info').innerHTML = 'lat: ' + lat + ' - lon: ' + lon;
+
+      initMap(lat, lon)
     },10)
-  },2000)
+  },10000)
+}
+
+function initMap(lat, lng) {
+  var myLatLng = {lat: lat, lng: lng};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('bus_1_map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 5
+  });
+
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng,
+    title: 'Hello World!'
+  });
 }
 
 
-startUpdater()
+startUpdater();
