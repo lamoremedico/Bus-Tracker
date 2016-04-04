2d6f3250687568f08cbb60b83983be619a8a8dc1
Nick <lamoremedico@gmail.com>
2016-04-04 15:22:51 -0400

    Tried to use geolocation and display map on bus driver file

--- bus/public/javascripts/geoloc.js
+++ bus/public/javascripts/geoloc.js
@@ -22,13 +22,21 @@ function geoFindMe() {
   function success(position) {
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
+    var myLatLng = {lat: latitude, lng: longitude};
 
     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
-    var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
-    output.appendChild(img);
+    var map = new google.maps.Map(document.getElementById('bus_27_map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 12
+  });
+
+    //var img = new Image();
+    //img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    //output.appendChild(img);
   };
 
   function error() {
